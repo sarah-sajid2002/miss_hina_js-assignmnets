55caef829636ac2bedff87728c666b487e612476
Sarah Sajid <sarahsajid19@gmail.com>
2023-04-03 20:23:04 +0500

    js alerts assigmnet question 2

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 // java script alerts assignment:
 
 //Q1) Write a script to greet your website visitor using JS alert box.
- alert("hello there !");
+//  alert("hello there !");
+
+
+//Q2) 
+alert("java script alert  \nerror! please enter a valid password")
