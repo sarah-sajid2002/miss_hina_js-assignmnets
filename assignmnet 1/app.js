15ecc3e337976937e4a91ac5250442688644ae6c
Sarah Sajid <sarahsajid19@gmail.com>
2023-04-04 21:41:58 +0500

    js assignmnet 1 alerts

---
+++ assignmnet 1/app.js
@@ -0,0 +1,20 @@
+// java script alerts assignment:
+
+//Q1) Write a script to greet your website visitor using JS alert box.
+//  alert("hello there !");
+
+
+//Q2) 
+// alert("java script alert  \nerror! please enter a valid password")
+
+
+//Q3)
+// alert("java script alert \n\nwelcome to JS land......\nhappy coding!")
+
+//Q4) 
+// alert("java script alert \n\nwelcome to JS land......");
+// alert("java script alert \n\nhappy coding!");
+alert:pop
+
+//Q5)
+// alert("java script alert \n\nhello... i can run JS through my web browser's console");
