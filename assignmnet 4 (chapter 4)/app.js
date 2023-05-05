8c1b1161e314616f615d696930f136150b2ba09c
sarah sajid <123872994+sarah-sajid2002@users.noreply.github.com>
2023-05-05 19:13:14 +0500

    Add files via upload

---
+++ assignmnet 4 (chapter 4)/app.js
@@ -0,0 +1,41 @@
+// ================= JS ASSIGNMENT 4================
+//        =========  QUESTION 1     ===============
+
+// let name = "sarah";  let age = 20; let height = "5.3f";
+
+
+// //        =========  QUESTION 2     ===============
+
+// // legal variables
+
+// let my_name = "sarah";
+// let age_1 = 19;
+// let grade = "seven";
+// let phoneNumber = "033322382";
+// let $marks = "100";
+
+// // illegal variables
+
+// let 1name = "sarah";  //number in start illegal
+// let my age = 10;  // space in between illegal
+// let function = "hello world";  //reserved keyword
+// let address# = "ndfdsfi397"; // use of # is not allowed
+// let marks+percentage = 32;  // use of + and all special char except $ , _ are not allowed
+
+// //        =========  QUESTION 2     ===============
+// let number = " number "
+// let _ = " _ ";
+// let $ = " & ";
+// let alphabet = " letters "
+// let sensitivity = "sensitive";
+// let keyword = "reserved keyword"
+
+// document.write( "<b> Rules for naming JS variables </b> <br><br>")
+
+// document.write("Variable names can only contain "+ number + ", "+ _  + ", "+ $  + "and " +  alphabet + ", For example :$my_1stVariable "+"<br>")
+
+// document.write("Variables must begin with a "+  number  + _  + ", "+ $  + "and " +  alphabet + ", For example :$name, _name or name <br>")
+
+// document.write("Variable names are case " + sensitivity + "<br> ");
+
+// document.write("Variable names should not be JS  "+ keyword)
