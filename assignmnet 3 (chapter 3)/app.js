a205ab39411eafb15af94804cf7ae99e0d573399
sarah sajid <123872994+sarah-sajid2002@users.noreply.github.com>
2023-05-05 18:50:05 +0500

    Add files via upload

---
+++ assignmnet 3 (chapter 3)/app.js
@@ -0,0 +1,34 @@
+// // ============== JS ASSIGNMNET 3 =====================
+// //       ======== QUESTION 1     =========
+
+// let age = 20;
+// alert("i am " + age + " years old");
+
+// //          ======== QUESTION 2     =========
+
+// let number_of_visits = 0;
+// let i = 0;
+// for(i=0;i<1000;i++){
+//     number_of_visits = number_of_visits+1;
+//     alert("you have visited this site " + number_of_visits + " times")
+// }
+
+// function visits(number_of_visits){
+//     return number_of_visits;
+// }
+
+
+// //           ======== QUESTION 3     =========
+
+// let birth_year = 2002;
+// document.write("m birth year is " + birth_year );
+// document.write("<br>")
+// document.write("data type of my declared variable is number")
+
+
+// //              ======== QUESTION 4     =========
+// let client_name = "sarah sajid";
+// let no_of_orders = 10;
+// let product_title = "pent";
+
+// document.write( "<b>" + client_name  + "</b>"+ " ordered " + "<b>"+ no_of_orders  + " "+ product_title +  "</b>(s)"+ " on xyz clothing store");
