f645f6c3beae08acaf6134e7f5c72240833dc653
sarah sajid <123872994+sarah-sajid2002@users.noreply.github.com>
2023-05-06 11:34:50 +0500

    Add files via upload

---
+++ assignment 2 (chapter 2)/app.js
@@ -0,0 +1,64 @@
+// // =================================ASSIGNMNET 2===============================
+
+// // =================================
+// //                      QUESTION 1
+// // ==================================
+// // 1. Declare a variable called username
+
+
+// let username;
+
+
+// // =================================
+// //                      QUESTION 2
+// // ==================================
+// //  2. Declare a variable called myName & assign to it a string
+// // that represents your Full Name
+
+
+// let myName = "sarah sajid";
+
+
+// // =================================
+// //                      QUESTION 3
+// // ==================================
+// // 2. Declare a variable called myName & assign to it a string
+// // that represents your Full Name
+
+// let message; 
+// message = "hello world";
+// alert(message);
+
+
+// // =================================
+// //                      QUESTION 4
+// // ==================================
+// // 4. Write a script to save student’s bio data in JS variables and
+// // show the data in alert boxes.
+
+// let student_name = "sarah";
+// let student_age = 21;
+// let qualification = "cerified mobile app developer"
+// alert(student_name);
+// alert(student_age + " years old");
+// alert(qualification)
+
+
+// =================================
+//                      QUESTION 4
+// ==================================
+// 5. Write a script to display the following alert using one JS
+// variable:
+// pizza
+// pizz
+// piz
+// piz
+// pi
+// p
+
+// let Name = "PIZZA";
+// function trim (name){
+//     while (name.length>=1){
+//         alert(name.slice(0,i))
+//     }
+// }
