8601da8733f3eea4df914b68065d88c5554bd10c
sarah sajid <123872994+sarah-sajid2002@users.noreply.github.com>
2023-05-15 23:08:49 +0500

    Add files via upload

---
+++ assignmnet 6 (chapter 6 - 9)/app.js
@@ -0,0 +1,85 @@
+// // =================question 1=======================
+
+// let a = +prompt("enter a number");
+// document.write("the value of a is  :  " , a , "<br>");
+// document.write("------------------------------<br>");
+// document.write("the value of ++a is  :  " , ++a , "<br>");
+// document.write("now the value of a is :  " , a , "<br><br><br>");
+
+// document.write("the value of a++ is :  " , a++ , "<br>");
+// document.write("now the value of a is :  " , a , "<br><br><br>");
+
+// document.write("the value of a-- is :  " , a-- , "<br>");
+// document.write("now the value of a is :  " , a , "<br>");
+
+
+
+
+// // =================question 2=======================
+// let a = +prompt("enter number 1");
+// let b = +prompt("enter number 2");
+// document.write("value of a is : " , a , "<br>");
+// document.write("value of b is : " , b , "<br>");
+// document.write("solving the expression for --a - --b + ++b + b--  <br><br>")
+// let step_1 = --a;
+// let step_2 = --b;
+// let step_3 = ++b;
+// let step_4 = b--;
+
+// document.write("--a is : ", step_1  , "<br>");
+// document.write("--b is : ", step_2  , "<br>");
+// document.write("++b is : ", step_3  , "<br>");
+// document.write("b-- is : ", step_4 , "<br>");
+
+// document.write("result for --a - --b + ++b + b-- is : ", step_1-step_2+step_3+step_4);
+
+
+
+
+
+// // =================question 3=======================
+// let my_name = prompt("enter your name ");
+// document.write("welcome " + my_name);
+
+
+
+
+// // =================question 4=======================
+
+// let number = prompt("enter a number:")
+// if (!number) {
+//     for (i = 0; i <= 10; i++) {
+//         document.write(5, " X ", i, " = ", 5 * i, " <br>");
+//     }
+// }
+// else{
+// number = +number;
+// for (i = 0; i <= 10; i++) {
+//     document.write(number, " X ", i, " = ", number * i, "<br>");
+// }
+// }
+
+
+
+// =================question 5=======================
+// let sub_names_1 = prompt("enter name of subject 1");
+// let sub_names_2 = prompt("enter name of subject 2");
+// let sub_names_3 = prompt("enter name of subject 3");
+// let marks_1 = +prompt("enter total marks of : ", sub_names_1) 
+// let marks_2 = +prompt("enter total marks of : ",sub_names_2) 
+// let marks_3 = +prompt("enter total marks of : " ,sub_names_3) 
+// let obt_marks_1 = +prompt("enter obtained marks of : ", sub_names_1)
+// let obt_marks_2 = +prompt("enter obtained marks of : ", sub_names_2)
+// let obt_marks_3 = +prompt("enter obtained marks of : ", sub_names_3)
+// let create_table = document.createElement("table")
+// var table = document.getElementsByTagName(table);
+// var row = table.insertRow(0);
+//   var cell1 = row.insertCell(0);
+//   var cell2 = row.insertCell(1);
+//   var cell3 = row.insertCell(2);
+
+
+
+  // cell1.innerHTML = "english";
+  // cell2.innerHTML = "urdu";
+
